50e086e6a288211aeeda6ad27148c43319f471b5
IAM-ELANGO <151847683+IAM-ELANGO@users.noreply.github.com>
2024-02-21 13:31:34 +0530

    uploaded

---
+++ sumofarraysmalloc.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    int* arr=(int*) malloc(n*sizeof(int));
+    printf("Enter the size of array:");
+    scanf("%d",&n);
+    printf("Enter the array elements:\n");
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",(arr+i));
+    }
+    int sum=0;
+    for(int i=0;i<n;i++)
+    {
+        sum+=*(arr+i);
+    }
+    printf("The sum of array elements is %d",sum);
+    return 0;
+}
